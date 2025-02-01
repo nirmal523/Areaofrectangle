66fb8266345957bb211f32540d7308531bd8d8ac
nirmal523 <kadanirmalkumar@gmail.com>
2025-02-01 18:37:19 +0530

    Add files via upload

---
+++ AreaRectangle.java
@@ -0,0 +1,7 @@
+public class AreaRectangle {
+    public static void main(String[] args) {
+        int height=10;
+        float width=10.9f;
+        System.out.println("the area of rectangle is " +height*width);
+    }
+}
